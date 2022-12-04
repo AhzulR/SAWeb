98757c58ed7ffa8a77ca112a41af232d13ac5527
Anugrah Ahzul Raihan <36320628+AhzulR@users.noreply.github.com>
2022-12-04 15:57:03 +0700

    Add files via upload

--- saweb.sql
+++ saweb.sql
@@ -12,7 +12,28 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
+
+-- Dumping database structure for sawebuas
+CREATE DATABASE IF NOT EXISTS `sawebuas` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `sawebuas`;
+
+-- Dumping structure for table sawebuas.login
+CREATE TABLE IF NOT EXISTS `login` (
+  `id` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `nim` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table sawebuas.login: ~1 rows (approximately)
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` (`id`, `email`, `username`, `password`, `nama`, `nim`) VALUES
+	(0000000001, 'admin@gmail.com', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Anugrah Ahzul Raihan', '212552018253880'),
+	(0000000017, 'ahzul@gmail.com', 'ahzul', '86470200e03317106e1b46bc2fc9b6f8', 'Anugrah Raihan', '212552018253880');
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
